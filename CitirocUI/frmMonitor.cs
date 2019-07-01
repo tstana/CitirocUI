40904446905c38d340d8c380944e5f3a5465104e
Theodor Stana <stana@kth.se>
2019-07-01 18:03:02 +0200

    Move writing frmMonitor text boxes to DataReady handler
    Removing the properties previously used should make the class easier
    to understand...

--- CitirocUI/frmMonitor.cs
+++ CitirocUI/frmMonitor.cs
@@ -35,110 +35,6 @@ namespace CitirocUI
         {
             set { label_ConnStatus.Text = value; }
         }
-
-        private UInt32 TelemetryTimestamp
-        {
-            set
-            {
-                textBox_timestamp.Invoke( new EventHandler(
-                    delegate
-                    {
-                        textBox_timestamp.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 HitCountMPPC1
-        {
-            set
-            {
-                textBox_hitCountMPPC1.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_hitCountMPPC1.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 HitCountMPPC2
-        {
-            set
-            {
-                textBox_hitCountMPPC2.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_hitCountMPPC2.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 HitCountMPPC3
-        {
-            set
-            {
-                textBox_hitCountMPPC3.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_hitCountMPPC3.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 HitCountOR32
-        {
-            set
-            {
-                textBox_hitCountOR32.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_hitCountOR32.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 VoltageFromHVPS
-        {
-            set
-            {
-                textBox_voltageFromHVPS.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_voltageFromHVPS.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 CurrentFromHVPS
-        {
-            set
-            {
-                textBox_currentFromHVPS.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_currentFromHVPS.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
-
-        private UInt32 TempFromHVPS
-        {
-            set
-            {
-                textBox_tempFromHVPS.Invoke(new EventHandler(
-                    delegate
-                    {
-                        textBox_tempFromHVPS.Text = value.ToString();
-                    }
-                    ));
-            }
-        }
         #endregion
 
         #region Methods
@@ -195,6 +91,7 @@ namespace CitirocUI
 
         private void commChannel_DataReady(object sender, DataReadyEventArgs e)
         {
+            // 1. Parse received data bytes...
             UInt32 timestamp = Convert.ToUInt32(System.Text.Encoding.ASCII.GetString(e.DataBytes, 11, 10));
             byte[] ch0_hit_count = BitConverter.GetBytes(BitConverter.ToUInt32(e.DataBytes, 23));
             byte[] ch16_hit_count = BitConverter.GetBytes(BitConverter.ToUInt32(e.DataBytes, 27));
@@ -214,14 +111,67 @@ namespace CitirocUI
                 Array.Reverse(hvps_current);
             }
 
-            TelemetryTimestamp = timestamp;
-            HitCountMPPC3 = BitConverter.ToUInt32(ch0_hit_count, 0);
-            HitCountMPPC2 = BitConverter.ToUInt32(ch16_hit_count, 0);
-            HitCountMPPC1 = BitConverter.ToUInt32(ch31_hit_count, 0);
-            HitCountOR32 = BitConverter.ToUInt32( ch21_hit_count, 0);
-            VoltageFromHVPS = BitConverter.ToUInt32(hvps_voltage, 0);
-            CurrentFromHVPS = BitConverter.ToUInt32(hvps_current, 0);
-            TempFromHVPS = BitConverter.ToUInt32(hvps_temp, 0);
+            // 2. Convert them into strings...
+            UInt32 hitCountMPPC3 = BitConverter.ToUInt32(ch0_hit_count, 0);
+            UInt32 hitCountMPPC2 = BitConverter.ToUInt32(ch16_hit_count, 0);
+            UInt32 hitCountMPPC1 = BitConverter.ToUInt32(ch31_hit_count, 0);
+            UInt32 hitCountOR32 = BitConverter.ToUInt32(ch21_hit_count, 0);
+            UInt32 voltageFromHVPS = BitConverter.ToUInt32(hvps_voltage, 0);
+            UInt32 currentFromHVPS = BitConverter.ToUInt32(hvps_current, 0);
+            UInt32 tempFromHVPS = BitConverter.ToUInt32(hvps_temp, 0);
+
+            // 3. Apply the values into the text boxes; use the Control.Invoke()
+            //    method, to make sure the writing is done inside the original
+            //    UI thread
+            textBox_timestamp.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_timestamp.Text = timestamp.ToString();
+                }
+            ));
+
+            textBox_hitCountMPPC1.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_hitCountMPPC1.Text = hitCountMPPC1.ToString();
+                }
+            ));
+            textBox_hitCountMPPC2.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_hitCountMPPC2.Text = hitCountMPPC2.ToString();
+                }
+            ));
+            textBox_hitCountMPPC3.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_hitCountMPPC3.Text = hitCountMPPC3.ToString();
+                }
+            ));
+            textBox_hitCountOR32.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_hitCountOR32.Text = hitCountOR32.ToString();
+                }
+              ));
+            textBox_voltageFromHVPS.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_voltageFromHVPS.Text = voltageFromHVPS.ToString();
+                }
+            ));
+            textBox_currentFromHVPS.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_currentFromHVPS.Text = currentFromHVPS.ToString();
+                }
+            ));
+            textBox_tempFromHVPS.Invoke(new EventHandler(
+                delegate
+                {
+                    textBox_tempFromHVPS.Text = tempFromHVPS.ToString();
+                }
+            ));
         }
         #endregion
     }
